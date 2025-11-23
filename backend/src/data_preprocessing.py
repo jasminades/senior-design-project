e5208778de2cbfabea1df220ec38df65a7349ded
jasmina <123311005+jasminades@users.noreply.github.com>
2025-11-23 20:01:46 +0100

    adding transfer learning model

--- backend/src/data_preprocessing.py
+++ backend/src/data_preprocessing.py
@@ -5,14 +5,28 @@ os.environ["TF_NUM_INTEROP_THREADS"] = "1"
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def create_data_generators(train_dir, val_dir=None, img_size=(128,128), batch_size=32, show_examples=False):
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        brightness_range=[0.7, 1.3],
+        validation_split=0.2
+    )
+
+    val_datagen = ImageDataGenerator(
         rescale=1./255,
         validation_split=0.2
     )
 
-    train_data = datagen.flow_from_directory(
+    train_gen = train_datagen.flow_from_directory(
         train_dir,
         target_size=img_size,
         batch_size=batch_size,
@@ -20,8 +34,8 @@ def create_data_generators(train_dir, val_dir=None, img_size=(128,128), batch_si
         subset='training'
     )
 
-    val_data = datagen.flow_from_directory(
-        train_dir,  
+    val_gen = val_datagen.flow_from_directory(
+        train_dir,
         target_size=img_size,
         batch_size=batch_size,
         class_mode='categorical',
@@ -29,15 +43,15 @@ def create_data_generators(train_dir, val_dir=None, img_size=(128,128), batch_si
     )
 
     if show_examples:
-        images, labels = next(train_data)
+        images, labels = next(train_gen)
         plt.figure(figsize=(10, 8))
         num_images = min(9, images.shape[0])
         for i in range(num_images):
             plt.subplot(3, 3, i+1)
             plt.imshow(images[i])
-            plt.title(list(train_data.class_indices.keys())[labels[i].argmax()])
+            plt.title(list(train_gen.class_indices.keys())[labels[i].argmax()])
             plt.axis('off')
         plt.tight_layout()
         plt.show()
 
-    return train_data, val_data
+    return train_gen, val_gen
